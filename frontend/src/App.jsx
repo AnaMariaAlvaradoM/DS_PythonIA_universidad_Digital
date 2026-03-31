e0d1773356a9ad3b510acc0d6c83d8bc5c1b5878
 <anaa_alvarad@hotmail.com>
2026-03-30 21:03:25 -0500

    Parte inciial front

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App(){
+  return(
+    <div className="min-h-screen bg-indigo-950 flex items-center justify-center">
+      <p className="text-white text-2xl front-bold">
+      Universidad digital</p>
+    </div>
+  )
+}
